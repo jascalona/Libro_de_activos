3cb0ad24240eac3b52ddace0c5bc272c7cd78e84
jaescalona <abrahamjose.1587@gmail.com>
2024-08-13 15:38:20 -0400

    new

--- Controller/prestamos/edit.php
+++ Controller/prestamos/edit.php
@@ -41,10 +41,18 @@
 
                                 <div class="container-form">
 
+                            <?php
+                                include './Controller/modificar_solicitud.php';
+                            
+                            ?>
+
+
                                 <form action="" method="POST">
 
+
+
+
                                 <?php
-                                    include "./Controller/prestamos/modificar_solicitud.php";
 
                                     while($datos=$sql->fetch_object()){ ?>
 
@@ -112,11 +120,10 @@
                                 
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                <button type="submit" name="btn-e" class="btn btn-primary" value="submit">Cargar Solicitud</button>
+                                <button type="submit" name="edit" class="btn btn-primary" value="submit">Cargar Solicitud</button>
                             </div>
 
-                        </form>
-
+                            </form>
                         </div>
 
 
--- Controller/prestamos/modificar_solicitud.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!empty($_POST['bn-e'])) {
-    header('location: https://www.youtube.com/watch?v=CSvFpBOe8eY');
-}
